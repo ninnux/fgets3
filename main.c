294b8875e99bfe515725c5eece5260ec357785d4
Nino Ciurleo <nino.ciurleo@garr.it>
2020-01-15 15:32:01 +0000

    gira, da controllare

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define MAX 10
 
 
-int main(void) {
+char* trovaparole(FILE* file, char*** parole,int* trovate){
     char line[STLEN];
     int index[MAX];
     int lens[MAX];
@@ -16,58 +16,75 @@ int main(void) {
     int k=0;
     int q=0;
     int reset=0;
-    char* parole[MAX];
+    char* result;
+    //char* parole[MAX];
 
     for(i=0;i<sizeof(lens);i++){
         lens[i]=0;
     }
+    for(i=0;i<sizeof(index);i++){
+        index[i]=0;
+    }
 
-    while (fgets(line, STLEN, stdin) != NULL && line[0] != '\n') {
-	    for(i=0;i<STLEN;i++){
-	    	if(line[i]==' ' && reset == 0){ //spazi all'inizio
-	    		reset=0;
-	    		continue;
-	    	}
-	    	if((line[i]==' '||line[i]=='\n') && reset == 1){ //spazio dopo parola
-	    		reset=0;
-                k++;
-                if(line[i]==' '){
-	    		    continue;
-                }
-                if(line[i]=='\n'){
-	    		  break;
-                }
-	    	}
-	    	if(reset!=0 && line[i]!=' '){ // durante la parola
-	    		lens[k]++;
-	    		continue;	
-	    	}
-	    	if(reset==0 && line[i]!=' '){ // inizio parola
-	    		index[k]=i;
-	    		lens[k]++;
-	    		reset=1;
-	    	}
+    result=fgets(line, STLEN, file);
+	for(i=0;i<STLEN;i++){
+		if(line[i]==' ' && reset == 0){ //spazi all'inizio
+			reset=0;
+			continue;
+		}
+		if((line[i]==' '||line[i]=='\n') && reset == 1){ //spazio dopo parola
+			reset=0;
+            k++;
+            if(line[i]==' '){
+			    continue;
+            }
+            if(line[i]=='\n'){
+			  break;
+            }
+		}
+		if(reset!=0 && line[i]!=' '){ // durante la parola
+			lens[k]++;
+			continue;	
+		}
+		if(reset==0 && line[i]!=' '){ // inizio parola
+			index[k]=i;
+			lens[k]++;
+			reset=1;
+		}
 
-	    }	
-            //puts(line);
-	    printf("line:%s i:%d\n",line,i);
-        for(q=0;q<k;q++){
-            parole[q]=malloc(lens[q]+1);
-            memcpy(parole[q],&line[index[q]],lens[q]);
-	        printf("parola trovata in posizione %d lunga %d parola:%s\n",index[q],lens[q],parole[q]);
-            free(parole[q]);
-	        //printf("parola trovata in posizione %d lunga %d\n",index[q],lens[q]);
-        }
-        reset=0;
-        k=0;
-        q=0;
-        for(i=0;i<sizeof(lens);i++){
-            lens[i]=0;
-        }
-        for(i=0;i<sizeof(index);i++){
-            index[i]=0;
+	}	
+	//printf("line:%s i:%d\n",line,i);
+    for(q=0;q<k;q++){
+        //printf("pippo\n");
+        parole[q]=malloc(lens[q]+1);
+        memcpy(parole[q],&line[index[q]],lens[q]);
+	    //printf("parola trovata in posizione %d lunga %d parola:%s\n",index[q],lens[q],parole[q]);
+        //free(parole[q]);
+    }
+    reset=0;
+    *trovate=k;
+    k=0;
+    q=0;
+    for(i=0;i<sizeof(lens);i++){
+        lens[i]=0;
+    }
+    for(i=0;i<sizeof(index);i++){
+        index[i]=0;
+    }
+    return result;
+}
+
+
+void main(){
+    char* p[MAX];
+    int trovate=0;
+    char* res;
+    while(trovaparole(stdin,(char***) p,&trovate)!=NULL){
+        printf("trovate nella linea: %d\n",trovate);
+        int t;
+        for(t=0;t<trovate;t++){
+            printf("parola trovata:%s\n",p[t]);
         }
     }
-    return 0;
 }
 
