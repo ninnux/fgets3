85492140e516ecd306756e5bab57a28655a02f43
Nino Ciurleo <nino.ciurleo@garr.it>
2020-01-15 13:16:57 +0000

    reset

--- main.c
+++ main.c
@@ -23,38 +23,50 @@ int main(void) {
     }
 
     while (fgets(line, STLEN, stdin) != NULL && line[0] != '\n') {
-	for(i=0;i<STLEN;i++){
-		if(line[i]==' ' && reset == 0){ //spazi all'inizio
-			reset=0;
-			continue;
-		}
-		if((line[i]==' '||line[i]=='\n') && reset == 1){ //spazio dopo parola
-			reset=0;
-            k++;
-			continue;
-		}
-		if(line[i]=='\n'){ // fine riga
-			break;
-		}
-		if(reset!=0 && line[i]!=' '){ // durante la parola
-			lens[k]++;
-			continue;	
-		}
-		if(reset==0 && line[i]!=' '){ // inizio parola
-			index[k]=i;
-			lens[k]++;
-			reset=1;
-		}
-	}	
+	    for(i=0;i<STLEN;i++){
+	    	if(line[i]==' ' && reset == 0){ //spazi all'inizio
+	    		reset=0;
+	    		continue;
+	    	}
+	    	if((line[i]==' '||line[i]=='\n') && reset == 1){ //spazio dopo parola
+	    		reset=0;
+                k++;
+                if(line[i]==' '){
+	    		    continue;
+                }
+                if(line[i]=='\n'){
+	    		  break;
+                }
+	    	}
+	    	if(reset!=0 && line[i]!=' '){ // durante la parola
+	    		lens[k]++;
+	    		continue;	
+	    	}
+	    	if(reset==0 && line[i]!=' '){ // inizio parola
+	    		index[k]=i;
+	    		lens[k]++;
+	    		reset=1;
+	    	}
 
-        //puts(line);
-	printf("line:%s i:%d\n",line,i);
-    }
-    for(q=0;q<k;q++){
-        parole[q]=malloc(lens[q]+1);
-        memcpy(parole[q],&line[index[q]],lens[q]);
-	    printf("parola trovata in posizione %d lunga %d parola:%s\n",index[q],lens[q],parole[q]);
-	    //printf("parola trovata in posizione %d lunga %d\n",index[q],lens[q]);
+	    }	
+            //puts(line);
+	    printf("line:%s i:%d\n",line,i);
+        for(q=0;q<k;q++){
+            parole[q]=malloc(lens[q]+1);
+            memcpy(parole[q],&line[index[q]],lens[q]);
+	        printf("parola trovata in posizione %d lunga %d parola:%s\n",index[q],lens[q],parole[q]);
+            free(parole[q]);
+	        //printf("parola trovata in posizione %d lunga %d\n",index[q],lens[q]);
+        }
+        reset=0;
+        k=0;
+        q=0;
+        for(i=0;i<sizeof(lens);i++){
+            lens[i]=0;
+        }
+        for(i=0;i<sizeof(index);i++){
+            index[i]=0;
+        }
     }
     return 0;
 }
